6fe5ce1dd1d129e4a629dc85b3f14c7e143e5e78
Vivianna Lieu <lieu.vivianna@gmail.com>
2021-08-03 12:14:29 -0500

    Initial Commit

---
+++ recycling-app-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recycling-app-project
+//
+//  Created by Scholar on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
