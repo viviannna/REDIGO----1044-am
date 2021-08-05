cfb24233f4522b133e09ed903663bd7cb0daf10d
Vivianna Lieu <lieu.vivianna@gmail.com>
2021-08-05 10:08:19 -0500

    added all the detail pages

---
+++ recycling-app-project/AluminumViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AluminumViewController.swift
+//  recycling-app-project
+//
+//  Created by Scholar on 8/5/21.
+//
+
+import UIKit
+
+class AluminumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
