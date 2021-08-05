d4810f5131fc4ece06748f7d6c83d7a3c6948a58
Vivianna Lieu <lieu.vivianna@gmail.com>
2021-08-05 11:04:16 -0500

    changed design for non recyclable items

--- recycling-app-project/ViewController.swift
+++ recycling-app-project/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var testBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
